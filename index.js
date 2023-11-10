d1f9f25f256f83d5df3832fe522d5fd0e68449d7
Trevor Heath <trevormheath@gmail.com>
2023-11-09 20:00:31 -0700

    now should work for website

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require("express")
+let app = new express();
+app.set("view engine", "ejs")
+app.use(express.static('public'));
+
+const knex = require("knex")({
+    client: "mysql",
+    connection: {
+        host:"paradisedonutcompany.c13xzbdqwvjw.us-east-2.rds.amazonaws.com",
+        user:"admin",
+        password:"Password1",
+        database:"paradisedonutcompany",
+        port:3306,
+    },
+});
+
+app.get("/",(req,res) => {
+    knex
+    .select()
+    .from("flavor")
+    .then((result) => {
+    console.log(result);
+    res.render("index", {aDonutList: result});
+    });
+    });
+    app.listen(3000);
+
